ab087a635141fe18c0f592178563570246e6d89a
kapsh <kapsh@kap.sh>
2016-01-25 05:51:26 +0300

    ZSH configs

---
+++ .zsh/directories2.zsh
@@ -0,0 +1,25 @@
+# Changing/making/removing directory
+
+unalias md
+unalias mkdir
+alias mkdir='nocorrect mkdir -p -v'
+alias md='mkdir'
+alias d='dirs -v | head -10'
+
+
+# mkdir & cd to it
+function mkcd() {
+	if [[ -n "$1" ]]; then
+		mkdir -p -v "$1" && cd "$1";
+	else
+		echo 'mkcd: missing name'
+	fi
+}
+
+function cdls() {
+	if [[ -n "$1" ]]; then
+		cd "$1" && ls;
+	else
+		echo 'cdls: missing name'
+	fi
+}
---
+++ .zsh/file-suffixes.zsh
@@ -0,0 +1,7 @@
+
+alias -s {avi,mpeg,mpg,mov,m2v}=mpv
+alias -s {odt,doc,sxw,rtf}=lowriter
+alias -s {pdf}=mupdf
+alias -s {jpg,png,svg,xpm,bmp}=feh
+alias -s {html,htm,xhtml}=google-chrome-stable
+alias -s {md,markdown}=mad
