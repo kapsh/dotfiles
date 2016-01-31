5052f971165b1d0a775b39baaf1bc842d5b34bc1
kapsh <kapsh@kap.sh>
2016-02-01 01:22:30 +0300

    Opening new terminal in current dir setup

--- .zsh/termite.zsh
+++ .zsh/termite.zsh
@@ -1,5 +1,9 @@
-# Workaround for using termite with old systems
 
 if [[ "$TERM" == "xterm-termite" ]]; then
+	# Workaround for using termite with old systems
 	export TERM="xterm"
+	# Set current dir (for opening new tab)
+	. /etc/profile.d/vte.sh
+	__vte_osc7
+
 fi
