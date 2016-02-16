27dd8a27182cbf1e00fc7981faca702cb04f6de1
kapsh <kapsh@kap.sh>
2016-02-16 23:44:26 +0300

    killall completion in zsh fixed

---
+++ .zsh/completion.zsh
@@ -0,0 +1,7 @@
+
+# Better completion for killall
+zstyle ':completion:*:processes' command 'NOCOLORS=1 ps -U $(whoami)|sed "/ps/d"'
+zstyle ':completion:*:processes' insert-ids menu yes select
+zstyle ':completion:*:processes-names' command 'NOCOLORS=1 ps xho command|sed "s/://g"'
+zstyle ':completion:*:processes' sort false
+
