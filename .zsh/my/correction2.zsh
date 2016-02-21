0eab715904e9a69e480f4a7385bb111d771b5b88
kapsh <kapsh@kap.sh>
2016-02-21 20:48:19 +0300

    cave nocorrect opts

--- .zsh/my/correction2.zsh
+++ .zsh/my/correction2.zsh
@@ -1,5 +1,7 @@
 if [[ "$ENABLE_CORRECTION" == "true" ]]; then
-	alias cave='nocorrect cave'
+	alias cave='nocorrect noglob cave'
+	alias cave-resolve='nocorrect noglob cave-resolve'
+	alias cave-remove='nocorrect noglob cave-remove'
 	alias git='nocorrect git'
 fi
 
