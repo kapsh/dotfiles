e6c4ff4ad78cf4aa09de3982358e77be04c70445
kapsh <kapsh@kap.sh>
2016-05-22 22:11:45 +0300

    * zsh: optimized completions;

--- .zsh/my/aliases.zsh
+++ .zsh/my/aliases.zsh
@@ -25,3 +25,11 @@ alias myip="curl ip.appspot.com"
 [[ "$(whence colordiff)" ]] && alias diff='colordiff'
 
 alias svim="vim -u ~/.svimrc"
+
+if [[ "$ENABLE_CORRECTION" == "true" ]]; then
+    alias cave='nocorrect noglob cave'
+    alias cave-resolve='nocorrect noglob cave-resolve'
+    alias cave-remove='nocorrect noglob cave-remove'
+    alias git='nocorrect git'
+fi
+
