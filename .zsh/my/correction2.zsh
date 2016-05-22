9e406111acae19efe39607df7e228340e50dcd13
kapsh <kapsh@kap.sh>
2016-05-22 22:11:45 +0300

    * zsh: optimized completions;

--- .zsh/my/correction2.zsh
+++
@@ -1,9 +0,0 @@
-if [[ "$ENABLE_CORRECTION" == "true" ]]; then
-    alias cave='nocorrect noglob cave'
-    alias cave-resolve='nocorrect noglob cave-resolve'
-    compdef cave-resolve=cave
-    alias cave-remove='nocorrect noglob cave-remove'
-    compdef cave-purge=cave
-    alias git='nocorrect git'
-fi
-
