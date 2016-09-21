91cfabea69dcf41754c32b5853b2f6c14ec11083
kapsh <kapsh@kap.sh>
2016-09-21 12:19:37 +0300

    - zsh: removed italic for stderred;

--- .zsh/my/stderred.zsh
+++ .zsh/my/stderred.zsh
@@ -2,9 +2,8 @@
 export LD_PRELOAD="/usr/local/lib64/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
 
 bold=$(tput bold || tput md)
-italic=$(tput sitm || tput ZH)
 red=$(tput setaf 1)
 export STDERRED_ESC_CODE=$(echo -e $italic$red)
 unset bold red italic
 
-export STDERRED_BLACKLIST="^(wget|bash|test.*)$"
+export STDERRED_BLACKLIST="^(screen|wget|bash|test.*)$"
