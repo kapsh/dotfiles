65279c5094ccd78f2fba3bc224a7431be23ee231
kapsh <kapsh@kap.sh>
2017-02-15 00:43:44 +0300

    zsh: LD_PRELOAD error w/o stderred fixed;

--- .zsh/my/stderred.zsh
+++ .zsh/my/stderred.zsh
@@ -1,10 +1,16 @@
 
-export LD_PRELOAD="/usr/local/lib64/libstderred.so${LD_PRELOAD:+:$LD_PRELOAD}"
+lib_path="/usr/local/lib64/libstderred.so"
 
-bold=$(tput bold || tput md)
-italic=$(tput sitm || tput ZH)
-red=$(tput setaf 1)
-export STDERRED_ESC_CODE=$(echo -e $red)
-unset bold red italic
+if [[ -x "${lib_path}" ]]; then
 
-export STDERRED_BLACKLIST="^(screen|wget|bash|sh|python.*|test.*)$"
+    export LD_PRELOAD="${lib_path}${LD_PRELOAD:+:$LD_PRELOAD}"
+
+    bold=$(tput bold || tput md)
+    italic=$(tput sitm || tput ZH)
+    red=$(tput setaf 1)
+    export STDERRED_ESC_CODE=$(echo -e $red)
+    unset bold red italic
+
+    export STDERRED_BLACKLIST="^(screen|wget|bash|sh|python.*|test.*)$"
+
+fi
