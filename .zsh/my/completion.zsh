3d829ab168223678d9392a519426d518b2d96747
kapsh <kapsh@kap.sh>
2017-02-15 00:57:28 +0300

    zsh: completions refactoring;

--- .zsh/my/completion.zsh
+++ .zsh/my/completion.zsh
@@ -5,14 +5,15 @@ zstyle ':completion:*:processes' insert-ids menu yes select
 zstyle ':completion:*:processes-names' command 'NOCOLORS=1 ps xho command|sed "s/://g"'
 zstyle ':completion:*:processes' sort false
 
-# Completions for cave-wrapper
-compdef _cave_cmd_resume cave-resume
-compdef _cave_cmd_resolve cave-resolve
-compdef _cave_cmd_resolve cave-remove
+# Completions for cave-wrappers
+compdef _cave_cmd_fix-linkage cave-fix-linkage
 compdef _cave_cmd_resolve cave-purge
+compdef _cave_cmd_resolve cave-remove
+compdef _cave_cmd_resolve cave-resolve
 compdef _cave_cmd_resolve cave-update-system
 compdef _cave_cmd_resolve cave-update-world
-compdef _cave_cmd_fix-linkage cave-fix-linkage
+compdef _cave_cmd_resume cave-resume
+compdef _cave_cmd_search cave-search
 # Workaround: break lazy loading of subcommands
 autoload +XUz _cave
 _cave 2>/dev/null
