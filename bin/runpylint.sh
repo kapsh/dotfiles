91c1bfb7fa59702cd2c178259c948285e5ec89f3
kapsh <kapsh@kap.sh>
2018-03-11 13:40:52 +0300

    pylint wrapper: try to detect correct python version to run

--- bin/runpylint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-venv="${1:?missing path to virtualenv}"
-shift
-
-hook_code="import os; act = os.path.join(\"$venv\", 'bin', 'activate_this.py');\
-    exec(open(act).read(), {'__file__': act})"
-python -m pylint --init-hook "$hook_code" "$@"
-
