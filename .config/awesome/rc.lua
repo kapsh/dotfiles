de03ec5200facce44685edc8a672a055983c8703
Alexander Kapshuna <kapsh@kap.sh>
2019-07-14 17:33:14 +0300

    awesome:  autostart

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -584,3 +584,12 @@ end)
 
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
+
+-- Autostart
+local xresources_name = "awesome.started"
+local xresources = awful.util.pread("xrdb -query")
+if not xresources:match(xresources_name) then
+    awful.util.spawn_with_shell("xrdb -merge <<< " .. "'" .. xresources_name .. ":true'")
+    -- Execute once for X server
+    os.execute("dex --environment Awesome --autostart")
+end
