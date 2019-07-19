d67b7bccb6637205f72d3f722eac6d2d4db77d86
Alexander Kapshuna <kapsh@kap.sh>
2019-07-20 01:08:50 +0300

    ipython: add more auto-imports

--- .ipython/profile_default/startup/my.py
+++ .ipython/profile_default/startup/my.py
@@ -1,5 +1,8 @@
 import contextlib
+import datetime
 import itertools
+import os
+import shutil
 from pathlib import Path
 
 from IPython.core.magic import register_line_magic
@@ -10,16 +13,21 @@ with contextlib.suppress(ImportError):
 with contextlib.suppress(ImportError):
     import pdir
 
+with contextlib.suppress(ImportError):
+    import requests
+
 
 @register_line_magic
 def copy(line):
     import pyperclip
+
     pyperclip.copy(eval(line))
 
 
 @register_line_magic
 def paste(line):
     import pyperclip
+
     return pyperclip.paste()
 
 
