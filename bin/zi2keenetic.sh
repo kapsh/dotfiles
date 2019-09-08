0c096e4ef9462f49f259c1455d4ac73a22471f5a
Alexander Kapshuna <kapsh@kap.sh>
2019-09-08 21:06:49 +0300

    bin: reformat with shfmt

--- bin/zi2keenetic.sh
+++ bin/zi2keenetic.sh
@@ -7,30 +7,29 @@ find_networks() {
 
 # Output command for network in suitable for keenetic's parser format.
 create_command() {
-    network="${1:?}"  # a.b.c.d/n
+    network="${1:?}" # a.b.c.d/n
     calculated="$(ipcalc "$network")"
     echo "route add $(find_address "$calculated") mask $(find_mask "$calculated") 0.0.0.0"
 }
 
 # Output from ipcalc => network prefix.
 find_address() {
-    sed -nE 's|.*Address:\s+([0-9.]+).*|\1|p' <<< "$1"
+    sed -nE 's|.*Address:\s+([0-9.]+).*|\1|p' <<<"$1"
 }
 
 # Output from ipcalc => subnet mask.
 find_mask() {
-    sed -nE 's|.*Netmask:\s+([0-9.]+).*|\1|p' <<< "$1"
+    sed -nE 's|.*Netmask:\s+([0-9.]+).*|\1|p' <<<"$1"
 }
 
 main() {
     dump="${1:?missing path to dump.csv}"
     output="${2:?missing output path}"
 
-    networks="$(find_networks < "$dump")"
+    networks="$(find_networks <"$dump")"
     while read -r addr; do
         create_command "$addr"
-    done <<< "$networks" > "$output"
+    done <<<"$networks" >"$output"
 }
 
 main "$@"
-
