51ebe504eb36b1a3e5cf5fc6cc1eb18962ff039b
Alexander Kapshuna <kapsh@kap.sh>
2019-09-12 20:52:32 +0300

    zsh: reformat man() and remove printf from it

--- .zsh/my/colored_man_pages.zsh
+++ .zsh/my/colored_man_pages.zsh
@@ -1,27 +1,26 @@
 # Based on https://github.com/ael-code/zsh-colored-man-pages
 
 # termcap
-# ks       make the keypad send commands
 # ke       make the keypad send digits
-# vb       emit visual bell
+# ks       make the keypad send commands
 # mb       start blink
 # md       start bold
 # me       turn off bold, blink and underline
-# so       start standout (reverse video)
 # se       stop standout
-# us       start underline
+# so       start standout (reverse video)
 # ue       stop underline
+# us       start underline
+# vb       emit visual bell
 
 man() {
-	command env \
-		LESS_TERMCAP_mb=$(printf "\e[1;34m") \
-		LESS_TERMCAP_md=$(printf "\e[1;34m") \
-		LESS_TERMCAP_me=$(printf "\e[0m") \
-		LESS_TERMCAP_se=$(printf "\e[0m") \
-		LESS_TERMCAP_so=$(printf "\e[1;43;30m") \
-		LESS_TERMCAP_ue=$(printf "\e[0m") \
-		LESS_TERMCAP_us=$(printf "\e[1;32m") \
-		PAGER="${commands[less]:-$PAGER}" \
-		man "$@"
+    command env \
+        LESS_TERMCAP_mb=$'\e[1;34m' \
+        LESS_TERMCAP_md=$'\e[1;34m' \
+        LESS_TERMCAP_me=$'\e[0m' \
+        LESS_TERMCAP_se=$'\e[0m' \
+        LESS_TERMCAP_so=$'\e[1;43;30m' \
+        LESS_TERMCAP_ue=$'\e[0m' \
+        LESS_TERMCAP_us=$'\e[1;32m' \
+        PAGER="${commands[less]:-$PAGER}" \
+        man "$@"
 }
-
