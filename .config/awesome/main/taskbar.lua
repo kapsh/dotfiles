a25d2715b39b959760116de194e997347539c38f
Alexander Kapshuna <kapsh@kap.sh>
2021-01-08 18:31:11 +0300

    awesome: cleanup duplicated code

--- .config/awesome/main/taskbar.lua
+++ .config/awesome/main/taskbar.lua
@@ -4,94 +4,45 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 local hotkeys_popup = require("awful.hotkeys_popup")
 
---  Menu
--- Create a launcher widget and a main menu
-myawesomemenu = {
+local myawesomemenu = {
     { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
     { "edit config", RC.settings.editor_cmd .. " " .. awesome.conffile },
     { "restart", awesome.restart },
     { "quit", function() awesome.quit() end }
 }
-
-mymainmenu = awful.menu({
+local mymainmenu = awful.menu({
     items = {
         { "awesome", myawesomemenu, beautiful.awesome_icon },
         { "terminal", RC.settings.terminal }
     }
 })
-
-mylauncher = awful.widget.launcher({
+local menubutton = awful.widget.launcher({
     image = beautiful.awesome_icon,
     menu = mymainmenu
 })
 
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
---  Wibar
--- Create a textclock widget
-mytextclock = wibox.widget.textclock(RC.settings.clock_format, 1)
-
--- Create a launcher widget and a main menu
-myawesomemenu = {
-    { "hotkeys", function() hotkeys_popup.show_help(nil, awful.screen.focused()) end },
-    { "edit config", RC.settings.editor_cmd .. " " .. awesome.conffile },
-    { "restart", awesome.restart },
-    { "quit", function() awesome.quit() end }
-}
-
-mymainmenu = awful.menu({
-    items = {
-        { "awesome", myawesomemenu, beautiful.awesome_icon },
-        { "terminal", RC.settings.terminal }
-    }
-})
-
-mylauncher = awful.widget.launcher({
-    image = beautiful.awesome_icon,
-    menu = mymainmenu
-})
-
--- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
-
---  Wibar
--- Create a textclock widget
-mytextclock = wibox.widget.textclock(RC.settings.clock_format, 1)
-
--- Create a wibox for each screen and add it
 awful.screen.connect_for_each_screen(function(s)
     -- Each screen has its own tag table.
     awful.tag({"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}, s, awful.layout.layouts[2])
 
     -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(RC.interact.layoutbox_buttons)
-
-    -- Create a taglist widget
+    s.mypromptbox = awful.widget.prompt() -- used for run prompt
+    local layoutindicator = awful.widget.layoutbox(s)
+    layoutindicator:buttons(RC.interact.layoutbox_buttons)
     s.mytaglist = awful.widget.taglist {
         screen  = s,
         filter  = awful.widget.taglist.filter.all,
         buttons = RC.interact.taglist_buttons
     }
-
-    -- Create a tasklist widget
     s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, RC.interact.tasklist_buttons)
 
-    -- Create the wibox
-    s.mywibox = awful.wibar({position = "top", screen = s})
-
-    -- Add widgets to the wibox
-    s.mywibox:setup {
+    local mywibox = awful.wibar({position = "top", screen = s})
+    mywibox:setup {
         layout = wibox.layout.align.horizontal,
         {
             -- Left widgets
             layout = wibox.layout.fixed.horizontal,
-            mylauncher,
+            menubutton,
             s.mytaglist,
             s.mypromptbox,
         },
@@ -100,10 +51,10 @@ awful.screen.connect_for_each_screen(function(s)
         {
             -- Right widgets
             layout = wibox.layout.fixed.horizontal,
-            mykeyboardlayout,
+            awful.widget.keyboardlayout(),
             wibox.widget.systray(),
-            mytextclock,
-            s.mylayoutbox,
+            wibox.widget.textclock(RC.settings.clock_format, 1),
+            layoutindicator,
         },
     }
 end)
