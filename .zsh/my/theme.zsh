90e7e1bcae5841244ceb5e6fc5cf6213848eb929
Alexander Kapshuna <kapsh@kap.sh>
2021-07-05 01:32:32 +0300

    zsh: tune pure prompt colors

--- .zsh/my/theme.zsh
+++ .zsh/my/theme.zsh
@@ -1,10 +1,17 @@
 # Tune and load theme
 
+# Settings for `pure` prompt
 if [[ "$TERM" == "linux" ]]; then
-    # Consider primitive fonts in tty.
+    # simpler arrow for tty
     PURE_PROMPT_SYMBOL=">"
 fi
-PURE_CMD_MAX_EXEC_TIME=30
+PURE_CMD_MAX_EXEC_TIME=10
+zstyle :prompt:pure:prompt:success color cyan
+zstyle :prompt:pure:execution_time color 218
+zstyle :prompt:pure:git:arrow color green
+zstyle :prompt:pure:git:branch color yellow
+zstyle :prompt:pure:git:dirty color red
 
-zplugin ice pick"async.zsh" src"pure.zsh"
+# Load `pure` prompt
+zplugin ice pick "async.zsh" src "pure.zsh"
 zplugin light sindresorhus/pure
