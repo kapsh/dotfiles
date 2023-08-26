0311c04017cfd2c854a518a7a3ee0b5124500555
Alexander Kapshuna <kapsh@kap.sh>
2023-08-27 01:56:58 +0300

    ipython: quit without confirmation

--- .jupyter/jupyter_qtconsole_config.py
+++ .jupyter/jupyter_qtconsole_config.py
@@ -2,3 +2,4 @@ c.JupyterQtConsoleApp.display_banner = False
 c.JupyterQtConsoleApp.hide_menubar = True
 c.ConsoleWidget.paging = "vsplit"
 c.JupyterWidget.syntax_style = "monokai"
+c.JupyterConsoleApp.confirm_exit = False
